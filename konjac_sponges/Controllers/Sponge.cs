4b1682b1bfb8cc1eeab0c10b4b6ead70d881bd4f
HetPatel <w0805024@myscc.ca>
2022-10-13 14:41:29 -0400

    Controller and basic files added, Readme updated.

---
+++ konjac_sponges/Controllers/Sponge.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace konjac_sponges.Controllers
+{
+    public class Sponge : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
